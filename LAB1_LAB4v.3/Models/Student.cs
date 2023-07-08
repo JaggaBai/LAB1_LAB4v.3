97c2ba3f4588f4f8e64b3858776bbcbe99fee350
Jagga Bai <dzagur@post.pl>
2023-07-08 10:45:29 +0200

    Dodaj pliki projektów.

---
+++ LAB1_LAB4v.3/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace LAB1_LAB4v._3.Models
+{
+    public class Student
+    {
+       
+            public Guid Id { get; set; }
+
+            public string? Name { get; set; }
+
+            public string? Surname { get; set; }
+        }
+    }
+
---
+++ LAB1_LAB4v.3/Models/StudentDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace LAB1_LAB4v._3.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Student> Student { get; set; } = null!;
+    }
+    
+}
